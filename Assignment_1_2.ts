7c1bb4b1ac0fb0b96e1a62228e8d8faa3e0d0369
firasqureshi <riz_qureshi@hotmail.com>
2023-08-07 18:39:38 +0500

    Assignment 1 and 2

---
+++ Assignment_1_2.ts
@@ -0,0 +1,11 @@
+/*Install Node.js, TypeScript and VS Code on your computer.
+Personal Message: Store a person’s name in a variable, and print a message to that person. 
+Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”*/
+
+
+//Assignment 1: Node.js, TypeScript and VS Code installed on Computer.
+
+//Assignment 2: See below
+
+let personName: string = 'Jahanzaib'; 
+console.log(`Hello ${personName}, would you like to learn some python today?`)
