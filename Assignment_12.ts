9ab6bae4e17483da3a97a3dee6ebfd5018a4e948
firasqureshi <riz_qureshi@hotmail.com>
2023-08-13 20:00:52 +0500

    Assignment 12

---
+++ Assignment_12.ts
@@ -0,0 +1,27 @@
+// Greetings: Start with the array you used in Exercise 11, 
+// but instead of just printing each person’s name, 
+// print a message to them. 
+// The text of each message should be the same, 
+// but each message should be personalized with the person’s name.
+
+export{};
+
+let fr_name = ["Ali" , "Ahmed" , "Asif" , "Muneer" , "Akram"];
+let fr_mess_start : string = "I just want to clarify that ";
+let fr_mess_end : string = " is one of my best friends.";
+
+// by calling each array element with its index in console.log separately.
+
+console.log(fr_mess_start+ fr_name[0] + fr_mess_end);
+console.log(fr_mess_start+ fr_name[1] + fr_mess_end);
+console.log(fr_mess_start+ fr_name[2] + fr_mess_end);
+console.log(fr_mess_start+ fr_name[3] + fr_mess_end);
+console.log(fr_mess_start+ fr_name[4] + fr_mess_end);
+
+
+console.log("-".repeat(20) + "Line End" + "-".repeat(30));
+
+// By using For loop
+ for (let i = 0 ; i < 5 ; i++){
+     console.log(fr_mess_start + fr_name[i] + fr_mess_end)
+ };
