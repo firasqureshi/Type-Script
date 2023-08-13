6e5c85ea9e28fb491ceec32e842499346674cfba
firasqureshi <riz_qureshi@hotmail.com>
2023-08-13 18:20:32 +0500

    Assignment 8

---
+++ Assignment_8.ts
@@ -0,0 +1,35 @@
+// You should create four lines that look like this:
+
+// console.log(5 + 3)
+
+// Your output should simply be four lines with the number 8 appearing once on each line.
+
+
+// By Adding "-" character multiple times as string
+
+console.log("-----------------------------");
+console.log(5 + 3);
+
+console.log("-----------------------------");
+console.log(10 - 2);
+
+console.log("-----------------------------");
+console.log(4 * 2);
+
+console.log("-----------------------------");
+console.log(8 / 1);
+console.log("-----------------------------");
+
+// By Adding "-" character with repeat
+console.log("-".repeat(20));
+console.log(5 + 3);
+
+console.log("-".repeat(20));
+console.log(10 - 2);
+
+console.log("-".repeat(20));
+console.log(4 * 2);
+
+console.log("-".repeat(20));
+console.log(8 / 1);
+console.log("-".repeat(20));
