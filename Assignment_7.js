a7aedb274d5f825d696e2dd23c545b00d8b86bc0
firasqureshi <riz_qureshi@hotmail.com>
2023-08-13 14:55:57 +0500

    Assignment 7

---
+++ Assignment_7.js
@@ -0,0 +1,17 @@
+"use strict";
+// Number Eight: Write addition, subtraction, multiplication, and division operations 
+// that each result in the number 8. 
+// Be sure to enclose your operations in print statements to see the results.
+Object.defineProperty(exports, "__esModule", { value: true });
+let num1 = 4;
+let num2 = 4;
+console.log("Addition Operation:", num1 + num2);
+num1 = 10;
+num2 = 2;
+console.log("Substraction Operation:", num1 - num2);
+num1 = 8;
+num2 = 1;
+console.log("Multiplication Operation:", num1 * num2);
+num1 = 8;
+num2 = 1;
+console.log("Division Operation:", num1 / num2);
---
+++ Assignment_7.ts
@@ -0,0 +1,26 @@
+// Number Eight: Write addition, subtraction, multiplication, and division operations 
+// that each result in the number 8. 
+// Be sure to enclose your operations in print statements to see the results.
+
+export{}
+
+let num1 :number = 4;
+let num2 :number = 4;
+
+console.log("Addition Operation:" , num1 + num2);
+
+
+num1 = 10;
+num2 = 2;
+
+console.log("Substraction Operation:" , num1 - num2);
+
+num1 = 8;
+num2 = 1;
+
+console.log("Multiplication Operation:" , num1 * num2);
+
+num1 = 8;
+num2 = 1;
+
+console.log("Division Operation:" , num1 / num2);
