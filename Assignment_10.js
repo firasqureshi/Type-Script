bfa03858b09b17e9645ae5c6e97754ae7633caba
firasqureshi <riz_qureshi@hotmail.com>
2023-08-13 18:55:01 +0500

    Assignment 10

---
+++ Assignment_10.js
@@ -0,0 +1,12 @@
+"use strict";
+// Adding Comments: Choose two of the programs you’ve written, 
+// and add at least one comment to each. 
+// If you don’t have anything specific to write because your programs are too simple at this point, 
+// just add your name and the current date at the top of each program file. 
+// Then write one sentence describing what the program does.
+Object.defineProperty(exports, "__esModule", { value: true });
+//Below program take two number and return their sum
+const sum = (num1, num2) => {
+    return num1 + num2;
+};
+console.log("The sum of two numbers is: ", sum(100, 109));
