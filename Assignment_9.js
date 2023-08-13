eb01d10c385075dd335b5fd9305bebbb6dc38c0a
firasqureshi <riz_qureshi@hotmail.com>
2023-08-13 18:49:58 +0500

    Assignment 9

---
+++ Assignment_9.js
@@ -0,0 +1,11 @@
+"use strict";
+// Favorite Number: Store your favorite number in a variable. 
+// Then, using that variable, create a message that reveals your favorite number. 
+// Print that message.
+Object.defineProperty(exports, "__esModule", { value: true });
+// simple string concatenation
+let fav_num = 111;
+console.log("My favorite number is ", fav_num);
+// storing concatenated string in a variable by sing template literals and then print that variable using
+let fav_num_mes = `My favorite number is ${fav_num}`;
+console.log(fav_num_mes);
---
+++ Assignment_9.ts
@@ -0,0 +1,14 @@
+// Favorite Number: Store your favorite number in a variable. 
+// Then, using that variable, create a message that reveals your favorite number. 
+// Print that message.
+
+export{}
+
+// simple string concatenation
+let fav_num : number = 111;
+console.log("My favorite number is " , fav_num);
+
+// storing concatenated string in a variable by sing template literals and then print that variable using
+let fav_num_mes :string = `My favorite number is ${fav_num}`;
+console.log(fav_num_mes);
+
