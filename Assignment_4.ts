5f17b55d1f7572c2c3391271c4f07009c09edf6c
firasqureshi <riz_qureshi@hotmail.com>
2023-08-13 14:13:56 +0500

    Assignment 4

---
+++ Assignment_4.ts
@@ -0,0 +1,14 @@
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. 
+// Your output should look something like the following, including the quotation marks:
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+export{}
+
+let author :string = "Albert Einstein";
+console.log(author);
+
+let quote :string = "A person who never made a mistake never tried anything new.";
+console.log(quote);
+
+// used template literals
+console.log(`${author} once said, "${quote}"`);
